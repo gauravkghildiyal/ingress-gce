06772af8584e6cca24f812026955ab76c18b2c3e
Gaurav Ghildiyal <gauravkg@google.com>
2024-03-11 21:52:28 -0700

    Do not consider forbidden errors within the PSC controlletrollers sync metrics

--- pkg/psc/controller.go
+++ pkg/psc/controller.go
@@ -741,7 +741,7 @@ func SvcAttachmentKeyFunc(namespace, name string) string {
 func filterError(err error) error {
 	var apiError *googleapi.Error
 	if errors.As(err, &apiError) {
-		if utils.IsHTTPErrorCode(apiError, http.StatusNotFound) || utils.IsHTTPErrorCode(apiError, http.StatusBadRequest) {
+		if utils.IsHTTPErrorCode(apiError, http.StatusNotFound) || utils.IsHTTPErrorCode(apiError, http.StatusBadRequest) || utils.IsHTTPErrorCode(apiError, http.StatusForbidden) {
 			return nil
 		}
 	}
